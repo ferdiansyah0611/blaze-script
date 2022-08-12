dd7b7aee9832c2550032d103bb1f8b41f8aec392
Ferdiansyah <ferdif9996@gmail.com>
2022-08-12 08:33:42 +0700

    [update]

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import "../global.d";
 import Apps from "@/Apps";
 import { createApp } from "@root/render";
 import { makeRouter, page } from "@root/plugin/router";
-import withError from "@root/plugin/error";
 // route
 import Index from "./route/Index";
 import PageExample from "./route/Page";
@@ -11,7 +10,6 @@ import NotFound from "./route/404";
 const app = new createApp("#app", Apps, {
     dev: import.meta.env.DEV,
 });
-app.use(withError());
 app.use(
     makeRouter("#route", {
         url: [page("/", Index), page("/page", PageExample), page("/page/:id", PageExample), page("", NotFound)],
